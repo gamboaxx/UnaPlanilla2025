e656475bbdc4a949ef108c31f4e6583c642565f8
gamboaxx <luis.gamboa.retana@est.una.ac.cr>
2025-03-24 21:02:53 -0600

    [UPD] Vista de empleado y css

--- src/main/java/cr/ac/una/unaplanilla2025/controller/EmpleadosController.java
+++ src/main/java/cr/ac/una/unaplanilla2025/controller/EmpleadosController.java
@@ -4,9 +4,19 @@
  */
 package cr.ac.una.unaplanilla2025.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import io.github.palexdev.materialfx.controls.MFXDatePicker;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import io.github.palexdev.materialfx.controls.MFXRadioButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyEvent;
 
 /**
  * FXML Controller class
@@ -15,16 +25,79 @@ import javafx.fxml.Initializable;
  */
 public class EmpleadosController extends Controller implements Initializable {
 
+    @FXML
+    private MFXTextField txtId;
+    @FXML
+    private MFXTextField txtNombre;
+    @FXML
+    private MFXTextField txtPApellido;
+    @FXML
+    private MFXTextField txtSApellido;
+    @FXML
+    private MFXTextField txtCedula;
+    @FXML
+    private MFXRadioButton rdbMasculino;
+    @FXML
+    private ToggleGroup tggGenero;
+    @FXML
+    private MFXRadioButton rdbFemenino;
+    @FXML
+    private MFXCheckbox chkAdministrador;
+    @FXML
+    private MFXCheckbox chkActivo;
+    @FXML
+    private MFXDatePicker dtpFIngreso;
+    @FXML
+    private MFXDatePicker dtpFSalida;
+    @FXML
+    private MFXTextField txtCorreo;
+    @FXML
+    private MFXTextField txtUsuario;
+    @FXML
+    private MFXPasswordField txtClave;
+    @FXML
+    private MFXButton btnNuevo;
+    @FXML
+    private MFXButton btnBuscar;
+    @FXML
+    private MFXButton btnEliminar;
+    @FXML
+    private MFXButton btnGuardar;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @Override
     public void initialize() {
     }
-    
+
+    @FXML
+    private void onKeyPressedTxtId(KeyEvent event) {
+    }
+
+    @FXML
+    private void onActionCheckAdministrador(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnNuevo(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnBuscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnEliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnGuardar(ActionEvent event) {
+    }
+
 }
