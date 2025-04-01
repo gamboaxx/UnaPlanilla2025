e4b6637f9e6724c48c660c5031726d03a7ec48ad
gamboaxx <luis.gamboa.retana@est.una.ac.cr>
2025-03-31 20:18:38 -0600

    Update TiposPlanillaController.java

--- src/main/java/cr/ac/una/unaplanilla2025/controller/TiposPlanillaController.java
+++ src/main/java/cr/ac/una/unaplanilla2025/controller/TiposPlanillaController.java
@@ -4,9 +4,14 @@
  */
 package cr.ac.una.unaplanilla2025.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 
 /**
  * FXML Controller class
@@ -14,6 +19,29 @@ import javafx.fxml.Initializable;
  * @author josue_5njzopn
  */
 public class TiposPlanillaController extends Controller implements Initializable {
+
+    @FXML
+    private MFXButton btnNuevo;
+    @FXML
+    private MFXButton btnBuscar;
+    @FXML
+    private MFXButton btnEliminar;
+    @FXML
+    private MFXButton btnGuardar;
+    @FXML
+    private MFXTextField txtId;
+    @FXML
+    private CheckBox chkActive;
+    @FXML
+    private MFXTextField txtCode;
+    @FXML
+    private MFXTextField txtDescription;
+    @FXML
+    private MFXTextField txtPlanXMes;
+    @FXML
+    private MFXTextField txtIdEmployee;
+    @FXML
+    private MFXTextField txtName;
 
     /**
      * Initializes the controller class.
@@ -25,6 +53,22 @@ public class TiposPlanillaController extends Controller implements Initializable
 
     @Override
     public void initialize() {
+    }
+
+    @FXML
+    private void onActionBtnNuevo(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnBuscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnEliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnGuardar(ActionEvent event) {
     }
     
 }
